c97fde520797f4be78eeca6ebe56105b202f2a45
Tom McCabe <btmccabejr@gmail.com>
2016-02-16 09:42:41 -0600

    push code with successful outbound connection

--- contrib/fedpeg/constants.py
+++ contrib/fedpeg/constants.py
@@ -6,7 +6,7 @@ class FedpegConstants:
 	# VARIOUS SETTINGS...
         user = os.environ["RPC_USER"]
         password = os.environ["RPC_PASS"]
-        sidechain_url = "http://" + user + ":" + password + "@127.0.0.1:4241"
+        sidechain_url = "http://" + user + ":" + password + "@127.0.0.1:4250"
         bitcoin_url = "http://" + user + ":" + password + "@127.0.0.1:18332"
 
 	redeem_script = "55210269992fb441ae56968e5b77d46a3e53b69f136444ae65a94041fc937bdb28d93321021df31471281d4478df85bfce08a10aab82601dca949a79950f8ddf7002bd915a2102174c82021492c2c6dfcbfa4187d10d38bed06afb7fdcd72c880179fddd641ea121033f96e43d72c33327b6a4631ccaa6ea07f0b106c88b9dc71c9000bb6044d5e88a210313d8748790f2a86fb524579b46ce3c68fedd58d2a738716249a9f7d5458a15c221030b632eeb079eb83648886122a04c7bf6d98ab5dfb94cf353ee3e9382a4c2fab02102fb54a7fcaa73c307cfd70f3fa66a2e4247a71858ca731396343ad30c7c4009ce57ae"
@@ -25,8 +25,8 @@ class FedpegConstants:
 	#Testnet:
 	#bitcoin_genesis_hash = "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"
 
-	nodes =["WARREN", "GWILLEN", "LUKE-JR/WIZKID", "MAAKU", "APOELSTRA", "MATT", "GMAXWELL"]
-	my_node = "FILL_ME_IN"
+	nodes =["68.232.186.219", "173.31.39.168"]
+	my_node = "68.232.186.219"
 
 	# Set this to non-None if you're using a proxy (eg for Tor)
 	# Note that this requires ZMQ 4.1
